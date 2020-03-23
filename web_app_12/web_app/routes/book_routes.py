8f5ba486e687c07ad311002e8012576da98e1c26
Wesley Mountford <goe_horus@hotmail.com>
2020-03-23 13:31:56 -0500

    Generated development database

--- web_app_12/web_app/routes/book_routes.py
+++ web_app_12/web_app/routes/book_routes.py
@@ -1,26 +1,20 @@
-# web_app/routes/book_routes.py
 
-from flask import Blueprint, jsonify, request, render_template #, flash, redirect
+from flask import Blueprint, jsonify, request, render_template, flash, redirect
+
+from web_app.models import db, Book, parse_records
 
 book_routes = Blueprint("book_routes", __name__)
 
 @book_routes.route("/books.json")
 def list_books():
-    books = [
-        {"id": 1, "title": "Book 1"},
-        {"id": 2, "title": "Book 2"},
-        {"id": 3, "title": "Book 3"},
-    ]
-    return jsonify(books)
+    book_records = Book.query.all()
+    books_response = parse_records(book_records)
+    return jsonify(books_response)
 
 @book_routes.route("/books")
 def list_books_for_humans():
-    books = [
-        {"id": 1, "title": "Book 1"},
-        {"id": 2, "title": "Book 2"},
-        {"id": 3, "title": "Book 3"},
-    ]
-    return render_template("books.html", message="Here's some books", books=books)
+    book_records = Book.query.all()
+    return render_template("books.html", message="Here's some books", books=book_records)
 
 @book_routes.route("/books/new")
 def new_book():
@@ -29,10 +23,15 @@ def new_book():
 @book_routes.route("/books/create", methods=["POST"])
 def create_book():
     print("FORM DATA:", dict(request.form))
-    # todo: store in database
-    return jsonify({
-        "message": "BOOK CREATED OK (TODO)",
-        "book": dict(request.form)
-    })
+
+    new_book = Book(title=request.form["title"], author_id=request.form["author_name"])
+    print(new_book)
+    db.session.add(new_book)
+    db.session.commit()
+
+    #return jsonify({
+    #    "message": "BOOK CREATED OK",
+    #    "book": dict(request.form)
+    #})
     #flash(f"Book '{new_book.title}' created successfully!", "success")
-    #return redirect(f"/books")
+    return redirect(f"/books")
