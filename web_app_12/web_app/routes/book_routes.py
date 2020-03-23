e47110a45637c6492641ae79119aef19cc667ed9
Wesley Mountford <goe_horus@hotmail.com>
2020-03-23 13:01:05 -0500

    Generated development database

--- web_app_12/web_app/routes/book_routes.py
+++ web_app_12/web_app/routes/book_routes.py
@@ -1,22 +1,30 @@
-#
+# web_app/routes/book_routes.py
 
-from flask import Blueprint, jsonify, render_template, flash, redirect, request
+from flask import Blueprint, jsonify, request, render_template #, flash, redirect
 
 book_routes = Blueprint("book_routes", __name__)
 
-@book_routes.route("/books")
-def about():
-    print("Visited book page")
+@book_routes.route("/books.json")
+def list_books():
     books = [
-        {"id": 1, "title":"Book 1"},
+        {"id": 1, "title": "Book 1"},
         {"id": 2, "title": "Book 2"},
-        {"id": 3, "title": "Book 3"}
+        {"id": 3, "title": "Book 3"},
     ]
-    return render_template("books.html", books=books)
+    return jsonify(books)
+
+@book_routes.route("/books")
+def list_books_for_humans():
+    books = [
+        {"id": 1, "title": "Book 1"},
+        {"id": 2, "title": "Book 2"},
+        {"id": 3, "title": "Book 3"},
+    ]
+    return render_template("books.html", message="Here's some books", books=books)
 
 @book_routes.route("/books/new")
 def new_book():
-    return render_template("new_books.html")
+    return render_template("new_book.html")
 
 @book_routes.route("/books/create", methods=["POST"])
 def create_book():
